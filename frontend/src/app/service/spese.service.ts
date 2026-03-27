646ae89c69c427a867a67c02ae4182fb239dd6e4
chris <34774499+cperoni@users.noreply.github.com>
2026-03-27 23:42:01 +0100

    WIP aggiunto service spese

---
+++ frontend/src/app/service/spese.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { supabase } from '../../lib/supabase.client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpeseService {
+
+  getSpese() {
+    return supabase
+      .from('spese')
+      .select('*')
+      .order('created_at', { ascending: false });
+  }
+
+  addSpesa(spesa: {
+    importo: number;
+    descrizione: string;
+    data: Date;
+    categoria_id: number;
+  }) {
+    return supabase.from('spese').insert([spesa]);
+  }
+
+  deleteSpesa(id: string) {
+    return supabase.from('spese').delete().eq('id', id);
+  }
+}
