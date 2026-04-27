0b9c1ef6a79c6f4da273be208478545cf76dd414
chris <34774499+cperoni@users.noreply.github.com>
2026-04-27 18:12:30 +0200

    fix feedback

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeedbackStack } from './shared/feedback/feedback-stack/feedback-stack';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FeedbackStack],
   templateUrl: './app.html',
 })
 export class App {}
